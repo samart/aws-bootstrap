a57ada265884f64194248476a54a597231f55be1
samart <trevor00@gmail.com>
2020-05-26 17:48:44 -0400

    Adding AWS Code build spec files

---
+++ deploy-infra.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'
+STACK_NAME=awsbootstrap
+REGION=us-east-1
+CLI_PROFILE=awsbootstrap
+
+EC2_INSTANCE_TYPE=t2.micro
+# shellcheck disable=SC2006
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket="$CODEPIPELINE_BUCKET"
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yaml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE
