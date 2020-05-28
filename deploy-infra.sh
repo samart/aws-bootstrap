bc98820f9fcdac5798f130c51f3c930809742d04
samart <trevor00@gmail.com>
2020-05-28 16:41:38 -0400

    added domain

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,6 +2,7 @@
 
 shopt -s expand_aliases
 alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'
+DOMAIN=evilcorp.cloud
 STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
@@ -67,6 +68,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    Domain=$DOMAIN \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
